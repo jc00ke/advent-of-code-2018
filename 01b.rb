b5f891ef12c9e3a4bfc050b9c230e327c40ded55
Jesse Cooke <jesse@jc00ke.com>
2018-12-02 09:19:34 -0800

    Day 1 - part 2

---
+++ 01b.rb
@@ -0,0 +1,24 @@
+require "set"
+
+def sum_of(start, lines, seen)
+  sum = lines.inject(start) do |a, line|
+    a += line.to_i
+
+    throw(:found, a.to_s) if seen.member?(a)
+
+    seen.add(a)
+    a
+  end
+
+  sum_of(sum, lines, seen)
+end
+
+lines = IO.readlines("01.txt", chomp: true)
+start = 0
+seen = Set[start]
+
+value = catch(:found) do
+  sum_of(start, lines, seen)
+end
+
+puts value
