9e9d82efd48b677f30b9a71b8df214f6b3c96d96
Jesse Cooke <jesse@jc00ke.com>
2018-12-02 21:58:18 -0800

    Day 2 - part 2

---
+++ 02/b.rb
@@ -0,0 +1,293 @@
+# frozen_string_literal: true
+
+require "set"
+require "jaro_winkler"
+
+set = SortedSet.new
+
+class Item
+  attr_reader :a, :b, :distance, :shared
+
+  def initialize(a, b)
+    @a = a
+    @b = b
+    @shared = shared_letters
+    @distance = JaroWinkler.distance(a, b)
+  end
+
+  def <=>(other)
+    @distance <=> other.distance
+  end
+
+  def shared_letters
+    s = []
+    @a.chars.each_with_index { |a, i|
+      s << a if a == @b[i]
+    }
+    s.join("")
+  end
+
+  def inspect
+    "#<Item: a=#{@a} b=#{@b} distance=#{@distance} shared=#{@shared}>"
+  end
+end
+
+DATA.map(&:chomp).combination(2) do |(a, b)|
+  set << Item.new(a, b)
+end
+
+max = set.max
+puts max.inspect
+puts max.shared_letters
+
+__END__
+oeambtcgjqnzhgkdylfapoiusr
+oewmbtcxjqnzhgvdyltapvqusr
+oewmbtchjqnzigkdylfapviuse
+oeimbucxjqnzhgkdyyfapviusr
+fewmbtcxjqndhgcdylfapviusr
+oevgbtccjqnzhgkdylfapviusr
+oewmbtcxjqnzhnkdylmapvpusr
+oewmbtcxjqnzhxkdyldapvirsr
+oewmutccjqnzngkdylfapviusr
+oewmbtcxbqnzhgkdsliapviusr
+ozwmbtfxjqnzcgkdylfapviusr
+oewmbtdxjqnzhgkdypfapsiusr
+oeylbtcxjqnzhgyyylfapviusr
+oewmbtcxjqnzhgkdrlfakuiusr
+oewmbtcujqnxhgkdylfadviusr
+oewmbtcxlqpzhgkdylfaaviusr
+oewmztcxjqnzhgkdylfqpliusr
+oeembtcxjqnzhgkdtlmapviusr
+onwmbtcxjqnqhgkdylfapdiusr
+oewmbtcxnqnzhgkdylfapsbusr
+oeoibtjxjqnzhgkdylfapviusr
+oxwmbtcxjqnzhgkdylfapcipsr
+oewmbtoxbqnzhgzdylfapviusr
+okwubtcxjqnzhgkdylfapiiusr
+oewmbtcxjqnzhgodylfapnicsr
+oewmitcxjqnzhgkdylfaphlusr
+oewmbtaxjqnzhgkhylfapveusr
+oewmftcbjqnzhgkdylfapviurr
+oewmbtcujqnzbgkdylfapliusr
+oeevbtcxjqnzhgkdylfapniusr
+oewmbtcxjqnvhgkdylfapnpusr
+oewabtcxjqnzhgddylfapviust
+oewmbtyxjqnzhgkdvlfapvinsr
+jewmbtcxjonzhzkdylfapviusr
+oewmbrcxjqnzxgkdylfapoiusr
+dewmbtmxjqnzhgkdyvfapviusr
+oewmbtctjqnzhgkdmlfapvihsr
+oewmbjcxjqnzhgvdylfapviurr
+oewmbtcxjqnzhgcdxlfapvfusr
+oewmbucxjqnzhgkdyltapvifsr
+gewmbtcejqezhgkdylfapviusr
+oewebtcxjznzhgkdylfapvhusr
+oewmjtcxjqnzhgkdycfakviusr
+oewmbtcxjtnvhgkdylfabviusr
+oewmbtcxjqnthgkgclfapviusr
+hewmbtcxjqnzhgkdwlfapziusr
+oewmbtcxjqnzhgkdylfqpviysf
+oewmbtcxjvnzhgmdylfapviuse
+oewmbtcxjqnphgkdymzapviusr
+oewmbtcxjqnzwmkdylfapvbusr
+oewmbthxjqnzhgkdylfatvilsr
+oewmbtcxaznzhgkhylfapviusr
+zewmbscxjqnzhgkdylfatviusr
+oewmbecyaqnzhgkdylfapviusr
+oewmbtnxjqnzhekxylfapviusr
+oewmbtcxjqczhgkdyltnpviusr
+yewmbecxjnnzhgkdylfapviusr
+oewmbocxjqnzhgkyylfapviusv
+oewmxtcxjqnzhgkkylfspviusr
+oiwmbtcxjqnzhgkdydfapvgusr
+oewmbtcxjqnzngydylfwpviusr
+oewmctcxjqnzhgkdelfapviasr
+oewmbtcxjqnzhgxdwmfapviusr
+oewmntcxjqnzhgkdylfamviusw
+oewmatcxjqbzhgkdylfapvhusr
+oewmbtcxjqnqhmkdyluapviusr
+opwmbtcxjqnzhgkdywfapvilsr
+omwmbtcxjqnlhgkdylyapviusr
+oewmltcxoqnzhgkdylfapvfusr
+oewmbtcxjqtzhgkdyyoapviusr
+oewmbtcxjqnzhrkdzlffpviusr
+oewmbtqxyqnzhgkdylfalviusr
+oeuzbtcxlqnzhgkdylfapviusr
+oewmbtcxjqnzhtxdylflpviusr
+oewmdtfxjqnzhgkdylfapviufr
+ojwmbtcxjqnzhgkdylfypviqsr
+oewmbtcxjqnzhgkdylfapvivuf
+oewmjtcsjqnzxgkdylfapviusr
+ohembtcxjqnzhnkdylfapviusr
+oewmptcajqnzhgkdylfapviusd
+oewmbtcxjcnwhgkbylfapviusr
+oewmbtcxjqnzhgddnlfapvqusr
+oewmbtcfjqnzhgkdypfapvzusr
+oewdbtccjqnzhgfdylfapviusr
+oewmbtcxjpnzhgkdplfaqviusr
+oepmbhcxjqnzhgkdylfaaviusr
+oewmbtcwjqxzhgkwylfapviusr
+oewmatcxjqnchgkdylfapvifsr
+omwmbncxjqnzhgkdylfapviuyr
+sewmbsckjqnzhgkdylfapviusr
+oewubtcxjqnzhgkdyluapvausr
+ohwmbtcxqqhzhgkdylfapviusr
+oewmbtcxjqnzhgkpylfapnissr
+eewmbccxjqnzhgkdylbapviusr
+oewmitcyjqnzhgkdylkapviusr
+oewmbtcxjvnzhgkdyjfvpviusr
+oewmbtcxjqmzhgkdyefagviusr
+oewmbtcvjqnzhgkdylpapviufr
+oewmbtcxjrnkhgkdylfapsiusr
+oewmbtcxjqnzygkdylfaxvipsr
+oexmbtcxjqczhgkdyloapviusr
+oewmbtcxjqnlhtkdylfapvmusr
+oewmbtcxdqjzdgkdylfapviusr
+oewmbtclgqnzhgkdylfabviusr
+oewmbtvfjqnzhgkdylfapviulr
+oewmbtcxjqnzhgkdyllarvijsr
+oewmbtyxjqnzhgpdylxapviusr
+oeylbtcxjqnzhgkyylfapviusr
+oewmbtctjqnzhjkdylfapviulr
+oermatcxjqnzhgkdylzapviusr
+oewmbtcxjqnztgkdzlfapviutr
+oewlbtcxjqnztgkvylfapviusr
+oewmbtcxjqzvhgkdylfapviusk
+oewmbtcxjqnzmgkdyldapvilsr
+felmbtcxjqnzhgkdylfapviusl
+oewmbtcxjgnzhgkjylfaeviusr
+ovwmbtcxjqpzhgkdylfapvizsr
+eewmbtcpjqnzhgkdylfapvijsr
+oewmbzcxjqnzhgkdylfaeviutr
+tewmbtcljqhzhgkdylfapviusr
+oewmbtcujqnzhgkdnliapviusr
+oewmbtcljqnzhskdylfapvgusr
+oewmbtchjqnzhgkdylmapviuse
+oewmbtcxjqnzbgkdylfaiviurr
+oewmbtcxjqnzhkkdyloapsiusr
+oewjbtcxjqnhhgkdylfapjiusr
+odwmbtcnjqnzhgkdylfapvicsr
+oewmbccxjqrzwgkdylfapviusr
+kewmbtcvjqnzhgkdylaapviusr
+okwmbtcxjqnzhgkdylfspvausr
+oewmbtcxjynzhgkdyafapviusw
+oewmbtcxjqnzhgwdyleayviusr
+oewmbtcxjqnzhgkdylfapviicl
+oewmbtcxjqnzhgkdyltaeziusr
+oewmbtcxrqnzhgkdylftpvizsr
+oewsrtcgjqnzhgkdylfapviusr
+oewmbtsxgqnzhgxdylfapviusr
+oewmbtcxjanzhgtdylfapeiusr
+oewybtcgjqnzhgkdylfapviust
+ojwmbncxjqnzhgkdylfapgiusr
+ocgebtcxjqnzhgkdylfapviusr
+oejcbtcxjqnzhgkvylfapviusr
+oswmbtcxjqnkhgkdylfapviusb
+oewdbtcxjqnzdgkdylfypviusr
+oawmutcxjqnzhgkddlfapviusr
+oewzbtcxyqnzhgkdylfapviusy
+zewmbtcxjqnzkgkdylwapviusr
+aewmbtkxjqnzhgkdylfapviuer
+oewmbtcxwqnzhgkdyofapviuur
+oewmbtcxjqnzggkdylfapanusr
+oewmstcxuqnzhgkdylzapviusr
+zewmbtcxjqozhgkdelfapviusr
+oewzbtcxjqnahgkdyllapviusr
+fewmatcxjqnghgkdylfapviusr
+oewmbtcxjqnzhgkdylfapviyqb
+oewwbtcxjqnzhgkdyljapviqsr
+oewmbtbxjqnzhgkxylfapviesr
+oewmbtcbjqnphgkdylfapviysr
+oewabtcxyqnzhgkdylfabviusr
+oewmbtcxhknzhgkdylfapviusd
+ozwmbtcljqnzhgkdylfapviksr
+tewmbtcxjqnzhgkdylfaxvqusr
+oewmbtcxrqnzhgkdytfapvrusr
+ohwmbtcxjcnzhgkdyifapviusr
+oewmbpcxjqnzhwkdylfaphiusr
+oedmbtcxjqnzhgnbylfapviusr
+oewmbocxjqnehgkdylfapvbusr
+oeymbtcxjqezegkdylfapviusr
+oewmbtcxjqnzhgkdllferviusr
+oewmbtcxjqnzhgkwmlfawviusr
+oewmbtcxienzhgkdyzfapviusr
+mewmbtcxjqnzhqkdylfapviwsr
+oewmbtcxjqnztgkmylfapvdusr
+ouwmbtcxjqnzhokdylpapviusr
+oewmctcxjqhzhgmdylfapviusr
+oewmbtcyjqnzhmkdylfarviusr
+oewmbtcxjqnzhgkdpnfzpviusr
+oewmptcxjqnzhgkdylkapviulr
+nefmbtcxsqnzhgkdylfapviusr
+oewmbtcxwqnzhgkdilfapvizsr
+eewmbtcxjqwzhghdylfapviusr
+oewmbtixmqnzhgkjylfapviusr
+okwmbtcdzqnzhgkdylfapviusr
+oewmbtxxjrnzigkdylfapviusr
+oewmdycxjqnzhekdylfapviusr
+oewmutcxjqnzhgkdylfapsiuqr
+oewmbacxjqnzrgkdrlfapviusr
+oewmbtcxpqnzhmkdylfapciusr
+oewabtcxjqnzhgkdyrcapviusr
+oswmbtcxjqnzhgkdrxfapviusr
+gewmbtcnjqnzhgkdylvapviusr
+newmbtcxjwnzfgkdylfapviusr
+lewmbtcxjqnzhgkdylfaptiujr
+oewwbtcxjqndhgkdylfapiiusr
+oewmbtcxjqnzhggdylfapvqmsr
+lewmbtcxjqnzhgkhllfapviusr
+oewmbtocjqnzhgkdylfapvhusr
+oedmbtcxjqnzhgkdyhfapviusb
+oewmbtcxjqnzhgkdylfajvaosr
+zewmbtcxjqnzhgkdylfapvsssr
+oewmbthxjqnzhskdylfapviuqr
+yewmrtcvjqnzhgkdylfapviusr
+oewmbtctjqnzhgkdylfabvhusr
+oesmstcxjqnzhgkdylfapqiusr
+oewmbtcxjqnzzgkdylfopiiusr
+otwmbtzxjqnzhgkdylfaxviusr
+ouwmbxcxjqnzhgkdylfapvnusr
+oewmbtcxjqezhgedylfapvsusr
+oesmhtcsjqnzhgkdylfapviusr
+oewdbtcxjqnzhgkdilfapvifsr
+oewmbtcxjqnzhgudynfamviusr
+qewhbtcxjqnzhgkdyxfapviusr
+oewmbzcxjqtzhgkdylfapvitsr
+oewmbtccjqzzhgkaylfapviusr
+jewmbtcxmqnzhlkdylfapviusr
+oewmbtcxjqbzhgkdnlfapviusp
+oeimbtcdjqnzhgkdylfapviuer
+oewtbtcxjqnihgkdylfahviusr
+oewmbtcxhqnzhgkdylfapdiudr
+oefmbtcxjqyshgkdylfapviusr
+oewmbtcxjqnzhgkfglfapviusx
+oecmbocxjqnzhgkdmlfapviusr
+oewmbtcxjqnzhghdylfavviuhr
+oewmbmcxiqnzhgkpylfapviusr
+oewmbtcnjqnzhgkrylfanviusr
+oewmbocxjqnzhzkdllfapviusr
+eewmbtckjqnzhgkdylfapviusg
+oewmbtcrjqqzhgkdylfapvigsr
+oewmbtcxjqazhgfdylfapjiusr
+oetmbtcxjqnzhgldylfapviqsr
+oewbbtcxjqnzlgkdylfapviuse
+oewmbtcxjqnzhglbolfapviusr
+oewmbtcxjqnzcgkdylfapviuhy
+oelmbtcxjqfzhgkdylaapviusr
+oojmbycxjqnzhgkdylfapviusr
+oewmbtrxjqnrhgkdylfapniusr
+oewmbtcxjqnzhgkyyhfapviuso
+oewabtcxjqnzhskdylfapviusx
+oewmbtcrjqnmhgkdylfapvnusr
+oewmbtcxjqrzhgkdylfapvpuss
+oewmbtcxhqnzwgkddlfapviusr
+kewmbtcxjqnzhgkyylfajviusr
+oswmbtcxjqnzhgkdjlfapviuss
+onwmbtcxjqnchgkdylfapvpusr
+oeymbtcxjqnxhikdylfapviusr
+oewmblcdjqnzhgkdylfapviysr
+oewmbtcxeqczhgudylfapviusr
+oewmbpgxjqnzhgkdylfapfiusr
+ohwmwtcxjqnzhgkdylftpviusr
+zebmbtuxjqnzhgkdylfapviusr
